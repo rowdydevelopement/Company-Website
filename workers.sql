39b7a87f2fda0133324df32354773127596f6d12
rowdypiperprogramming <s2005493@online.houstonisd.org>
2025-04-23 10:10:16 -0500

    Create workers.sql

---
+++ workers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE workers (
+    id INT,
+    name VARCHAR(30),
+    username VARCHAR(30),
+    langs VARCHAR(100)
+);
+
+INSERT INTO workers (id, name, username, langs)
+VALUES (1, 'Rowdy Piper', 'rowdypiperprogramming', 'py, sql, html,bash');
+
+INSERT INTO workers (id, name, username, langs)
+VALUES (2, 'Ryan Liu', 'rpdsworker1', 'py');
+
+INSERT INTO wokers (id, name, username, langs)
+VALUES (3, 'Jess McClesky',NULL,'JS, HTML');
